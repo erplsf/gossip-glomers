5100fe90b5219981787d88d9293b455c7e071dae
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-10-22 15:38:24 +0200

    add second workload

--- build.zig
+++ build.zig
@@ -24,25 +24,36 @@ pub fn build(b: *std.Build) void {
     });
     node.addImport("common", common);
 
-    const exe = b.addExecutable(.{
+    const echo_exe = b.addExecutable(.{
         .name = "echo",
         .root_source_file = b.path("src/echo.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("common", common);
-    exe.root_module.addImport("node", node);
+    const unique_ids_exe = b.addExecutable(.{
+        .name = "unique-ids",
+        .root_source_file = b.path("src/unique-ids.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    echo_exe.root_module.addImport("common", common);
+    echo_exe.root_module.addImport("node", node);
+
+    unique_ids_exe.root_module.addImport("common", common);
+    unique_ids_exe.root_module.addImport("node", node);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
-    b.installArtifact(exe);
+    b.installArtifact(echo_exe);
+    b.installArtifact(unique_ids_exe);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(echo_exe);
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
@@ -62,17 +73,17 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+    const echo_exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/echo.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_echo_exe_unit_tests = b.addRunArtifact(echo_exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_echo_exe_unit_tests.step);
 }
